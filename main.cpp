e960bbb360e60ee212826e6bd9694892eb2ee7e3
unknown <3065190005@qq.com>
2023-11-28 19:12:14 +0800

    StdArray weak type -> strong type

--- main.cpp
+++ main.cpp
@@ -221,38 +221,43 @@ R"(//--- debug
 require ("io");
 let io = new StdIo;
 
-require ("string");
-let string = new StdString;
+require ("array");
+let array = new StdArray;
 
-let str = "  Hello World : Begin {name} over  ";
-str = string.tirm(str);
-str = string.replace(str,"{name}","ZhangSan",string.str_all);
-let cutArr = string.cut(str,":");
-for i in cutArr:
-io.println(string.tirm(i));
-end
+let value = 0;
+value[0] = 10;
+value[1] = 11;
+value[2] = 12;
+value[3] = 10;
+value[4] = 10;
+value["Name"] = 13;
+value["Age"] = 14;
 
-let revertStr = string.reverse(str);
-io.println(revertStr);
+let value2 = ["ZhangSan","LiSi","WangWu"];
 
-let atStr = string.at(str,0) + string.at(str,1);
-io.println(atStr);
+let value3 = [100,200,300];
 
+io.println(value);
+io.println(value2);
 
-function log(val):
-	io.println(val);
-end
-	
-	log(string.isAlpha("abcAAfg"));
-	log(string.isAlnum("123456"));
-	log(string.hasBlank("123 455"));
-	log(string.isBlank("    "));
-	log(string.hasCntrl("123\n"));
-	log(string.isCntrl("\t\n"));
-	log(string.isLower("abcefg"));
-	log(string.isUpper("AAFEGSD"));
+io.println(array.size(value));
+io.println(array.cmp(value,value2));
+io.println(array.max(value));
+io.println(array.min(value));
+io.println(array.append(value,"HAHA"));
+io.println(array.count(value,10));
+io.println(array.extend(value,value2));
+io.println(array.back(value));
+io.println(array.front(value));
+io.println(array.index(value,14));
+io.println(array.insert(value,1,value3));
+io.println(array.pop(value3,0));
+io.println(array.remove(value3,200));
+io.println(array.reverse(value));
+io.println(array.range(1,10));
+io.println(array.childs(value));
 
-return;
+return 0;
 )";
 
 
