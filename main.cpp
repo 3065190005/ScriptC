4549d7ec5b4ff6fb2f5fc092525042c6215fe535
unknown <3065190005@qq.com>
2023-11-28 18:40:36 +0800

    StdString Weak type ->Strong type

--- main.cpp
+++ main.cpp
@@ -218,24 +218,41 @@ void TestCodeCall() {
 
 	std::string input = 
 R"(//--- debug
-require ("math");
-let math = new StdMath;
-
 require ("io");
 let io = new StdIo;
 
-let value = -1143.777;
-io.println(value);
+require ("string");
+let string = new StdString;
 
-value = math.abs(value);
-io.println(value);
-
-let arr = math.div(35,2);
-for i in arr:
-	io.println(i);
+let str = "  Hello World : Begin {name} over  ";
+str = string.tirm(str);
+str = string.replace(str,"{name}","ZhangSan",string.str_all);
+let cutArr = string.cut(str,":");
+for i in cutArr:
+io.println(string.tirm(i));
 end
 
-return 0;
+let revertStr = string.reverse(str);
+io.println(revertStr);
+
+let atStr = string.at(str,0) + string.at(str,1);
+io.println(atStr);
+
+
+function log(val):
+	io.println(val);
+end
+	
+	log(string.isAlpha("abcAAfg"));
+	log(string.isAlnum("123456"));
+	log(string.hasBlank("123 455"));
+	log(string.isBlank("    "));
+	log(string.hasCntrl("123\n"));
+	log(string.isCntrl("\t\n"));
+	log(string.isLower("abcefg"));
+	log(string.isUpper("AAFEGSD"));
+
+return;
 )";
 
 
